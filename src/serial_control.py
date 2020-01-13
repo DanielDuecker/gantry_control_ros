37105625eea159d064f9090f6f924602b1b46003
Daniel Duecker <daniel.duecker@tuhh.de>
2020-01-13 10:57:55 +0100

    catch exception for value errors in gantry serial communication

--- src/serial_control.py
+++ src/serial_control.py
@@ -133,16 +133,22 @@ class GantryCommunication(object):
             if last_string[-1] == '':
                 return False
             else:
-                x_pos_mm = int(last_string[0])
-                x_vel_mms = int(last_string[1])
-                y_pos_mm = int(last_string[2])
-                y_vel_mms = int(last_string[3])
-                z_pos_mm = int(last_string[4])
-                z_vel_mms = int(last_string[5])
+                try:
+                    x_pos_mm = int(last_string[0])
+                    x_vel_mms = int(last_string[1])
+                    y_pos_mm = int(last_string[2])
+                    y_vel_mms = int(last_string[3])
+                    z_pos_mm = int(last_string[4])
+                    z_vel_mms = int(last_string[5])
 
-                self.__gantry_pos_m = np.array([float(x_pos_mm)/1000, float(y_pos_mm)/1000, float(z_pos_mm)/1000])
-                self.__gantry_vel_ms = np.array([float(x_vel_mms)/1000, float(y_vel_mms)/1000, float(z_vel_mms)/1000])
-                return True
+                    self.__gantry_pos_m = np.array([float(x_pos_mm)/1000, float(y_pos_mm)/1000, float(z_pos_mm)/1000])
+                    self.__gantry_vel_ms = np.array([float(x_vel_mms)/1000, float(y_vel_mms)/1000, float(z_vel_mms)/1000])
+                    return True
+                
+                except ValueError:
+                    print("EXCEPTION: Detected ValueError in received serial stream - reject and and take next")
+                    print("Broken String" + last_string)
+                    return False
         return False
 
     def update_gantry_data(self):
